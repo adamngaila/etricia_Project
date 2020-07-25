5eed2321a6c34e4952fb2bc2c85d8f873e8ece00
adamngaila <adamngaila@gmail.com>
2020-07-25 16:57:41 +0300

    CHAMGES ON MIDDLEWARE

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'profile'=> \App\Http\Middleware\Adminm::class,
+        'profile'=> \App\Http\Middleware\AdminM::class,
     ];
 
     /**
