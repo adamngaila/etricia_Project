52a97fb844fe29e747df4ea9d76ee5caa8663642
ADAM MWANGAILA <adamngaila@gmail.com>
2022-01-01 00:40:06 +0300

    http method changed

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 //Route::post('/registration/', 'RegistrationController@store');
 Route::post('/SaveParams',"API\PowerpackAPI@save");
-Route::post('/profile_user',"API\PowerpackAPI@ProfileShow");
+Route::get('/profile_user',"API\PowerpackAPI@ProfileShow");
 Route::post('/add_user',"API\userAPI@AddUser");
 Route::post('/login_user',"API\userAPI@login");
 Route::middleware('auth:airlock')->post('/logout_user', function (Request $request) {
