eb336bcd5ee5e21df806b2c347983d05c1f4ae82
ADAM MWANGAILA <adamngaila@gmail.com>
2022-01-01 17:06:22 +0300

    method fetch improved

--- app/Http/Controllers/API/PowerpackAPI.php
+++ app/Http/Controllers/API/PowerpackAPI.php
@@ -23,7 +23,7 @@ class PowerpackAPI extends Controller
     }
     public function PackParametersView(Request $request)
     {
-       $ETParams = PowerpackParameters::where('packagecode', $request->code)->get();
+       $ETParams = PowerpackParameters::latest()->where('packagecode', $request->code)->take(-3)->get();
 
         $response = json_encode($ETParams);
 
