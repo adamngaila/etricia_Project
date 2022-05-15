e5eeeecea2fdcc994cdb7dda1939fa75a892b8bf
ADAM MWANGAILA <adamngaila@gmail.com>
2022-05-15 20:38:53 +0300

    new ROUTE

--- app/Http/Controllers/PowerpackController.php
+++ app/Http/Controllers/PowerpackController.php
@@ -18,7 +18,7 @@ class PowerpackController extends Controller
 
     public function DrawCharts()
     {
-        $query_id = Auth::user()->id;
+        $query_id = Auth::user()->serverip;
         $result = \DB::table('powerpack_packages')
                             ->join('powerpack_parameters','powerpack_packages.packagecode','=','powerpack_parameters.packagecode')
                             ->select('powerpack_packages.*','powerpack_parameters.*')
@@ -30,12 +30,12 @@ class PowerpackController extends Controller
 
     public function FetchDescription()
     {
-        $query_id = Auth::user()->id;
+        $query_id = Auth::user()->serverip;
         $result = \DB::table('powerpack_packages')
                             ->join('powerpack_parameters','powerpack_packages.packagecode','=','powerpack_parameters.packagecode')
                             ->join('etricia_directories','powerpack_packages.packagecode','=','etricia_directories.packagecode')
                             ->select('powerpack_packages.*','powerpack_parameters.*')
-                            ->where('user_id',$query_id)->get();                         
+                            ->where('packagecode',$query_id)->get();                         
          return redirect('/etricia')->with('result',$result);
     }
 
