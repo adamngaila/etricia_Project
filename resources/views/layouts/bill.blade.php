d2c60da4d45ae86bcb63789d2e1d4b6c885ccdef
ADAM MWANGAILA <adamngaila@gmail.com>
2022-08-20 18:41:04 +0300

    new

--- resources/views/layouts/bill.blade.php
+++ resources/views/layouts/bill.blade.php
@@ -32,8 +32,7 @@
   <!-- CSS Files -->
   <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
   <link href="../assets/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
-  <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="../assets/demo/demo.css" rel="stylesheet" />
+<link href="../assets/css/treestyle.css" rel="stylesheet" />
 </head>
 
 <body class="">
