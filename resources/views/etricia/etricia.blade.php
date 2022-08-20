397f4f494a1c2f3d006a072fcff9ebaf16878849
ADAM MWANGAILA <adamngaila@gmail.com>
2022-08-20 17:16:05 +0300

    new

--- resources/views/etricia/etricia.blade.php
+++ resources/views/etricia/etricia.blade.php
@@ -267,7 +267,22 @@ Etricia | Monitoring
 
  <div id="Diagnosis" class="tabcontent">
   <h5 class="title">Diagnosis</h5>
- 
+ <table class="table table-striped table-bordered">
+                <thead>
+                  <tr>
+                    <th> Free Resource </th>
+                    <th> Download</th>
+                    <th class="td-actions"> </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td> Fresh Web Development Resources </td>
+                    <td> http://www.egrappler.com/ </td>
+                    <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
+                  </tr>
+              </tbody>
+          </table>
 </div>
 
 
