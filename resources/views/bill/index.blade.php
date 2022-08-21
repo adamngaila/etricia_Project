64afc8694b584aee0b8cd49d115974192ddd4fe1
ADAM MWANGAILA <adamngaila@gmail.com>
2022-08-21 17:58:22 +0300

    new

--- resources/views/bill/index.blade.php
+++ resources/views/bill/index.blade.php
@@ -45,7 +45,7 @@ Etricia | Bills
             </div>
              <div id="statement" class="tabcontent">
               <h5 class ="title">Statement</h5>
-      <address contenteditable>
+      <address  disabled>
         <p>{{ Auth::user()->name }}</p>
         <p>{{ Auth::user()->region }}<br>{{ Auth::user()->location}}</p>
         <p>{{ Auth::user()->phone }}</p>
@@ -55,55 +55,54 @@ Etricia | Bills
    
     <article>
       <h1>Recipient</h1>
-      <address contenteditable>
+      <address >
         <p>{{ Auth::user()->serveip }}</p>
       </address>
       <table class="meta">
         <tr>
-          <th><span contenteditable>Statement #</span></th>
-          <td><span contenteditable>101138</span></td>
+          <th><span >Statement #</span></th>
+          <td><span >101138</span></td>
         </tr>
         <tr>
-          <th><span contenteditable>Date</span></th>
-          <td><span contenteditable>{{ date("Y/m/d")}}</span></td>
+          <th><span >Date</span></th>
+          <td><span >{{ date("Y/m/d")}}</span></td>
         </tr>
         <tr>
-          <th><span contenteditable>Amount Due</span></th>
-          <td><span id="prefix" contenteditable>$</span><span>600.00</span></td>
+          <th><span >Amount Due</span></th>
+          <td><span id="prefix" >$</span><span>600.00</span></td>
         </tr>
       </table>
       <table class="inventory">
         <thead>
           <tr>
-            <th><span contenteditable>Item</span></th>
-            <th><span contenteditable>Description</span></th>
-            <th><span contenteditable>Rate</span></th>
-            <th><span contenteditable>Quantity</span></th>
-            <th><span contenteditable>Price</span></th>
+            <th><span >Item</span></th>
+            <th><span >Description</span></th>
+            <th><span >Rate</span></th>
+            <th><span >Quantity</span></th>
+            <th><span >Price</span></th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td><a class="cut">-</a><span contenteditable>Front End Consultation</span></td>
-            <td><span contenteditable>Experience Review</span></td>
+            <td><a class="cut">-</a><span >Front End Consultation</span></td>
+            <td><span >Experience Review</span></td>
             <td><span data-prefix>$</span><span contenteditable>150.00</span></td>
-            <td><span contenteditable>4</span></td>
+            <td><span >4</span></td>
             <td><span data-prefix>$</span><span>600.00</span></td>
           </tr>
         </tbody>
       </table>
-      <a class="add">+</a>
+     
       <table class="balance">
-        <tr>
-          <th><span contenteditable>Total</span></th>
+        <tr>Total</span></th>
           <td><span data-prefix>$</span><span>600.00</span></td>
         </tr>
         <tr>
-          <th><span contenteditable>Amount Paid</span></th>
+          <th><span >Amount Paid</span></th>
           <td><span data-prefix>$</span><span contenteditable>0.00</span></td>
         </tr>
         <tr>
-          <th><span contenteditable>Balance Due</span></th>
+          <th><span >Balance Due</span></th>
           <td><span data-prefix>$</span><span>600.00</span></td>
         </tr>
       </table>
