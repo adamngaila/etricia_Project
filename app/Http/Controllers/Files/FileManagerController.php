dc9875f67aa000535c018c2fb77beb0c7a59aebd
ADAM MWANGAILA <adamngaila@gmail.com>
2022-09-03 19:20:37 +0300

    new

--- app/Http/Controllers/Files/FileManagerController.php
+++ app/Http/Controllers/Files/FileManagerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Files;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Response;
 class FileManagerController extends Controller
 {
     public function Download_App(){
