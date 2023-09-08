2a173f05703efb8533c7f0cadddf795354ac15a0
ADAM MWANGAILA <adamngaila@gmail.com>
2023-09-09 00:38:51 +0300

    new updates

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -103,7 +103,7 @@
 
 <script>
     // Map initialization 
-    var map = L.map('map').setView([-6.8395, 39.2894], 13);
+    var map = L.map('map').setView([-6.8195, 39.2894], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
