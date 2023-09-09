871232ae98d04e35f9728f378125beb09f3f6942
ADAM MWANGAILA <adamngaila@gmail.com>
2023-09-10 00:40:40 +0300

    new updates

--- app/Http/Controllers/API/PowerpackAPI.php
+++ app/Http/Controllers/API/PowerpackAPI.php
@@ -79,7 +79,7 @@ class PowerpackAPI extends Controller
             'ChargeLevel' => $ChargeLevel,
             'Grid' => $Grid,
         ]);
-         return response('liveupdate ok',201);
+         return response('liveupdate ok',200);
     }
     public function saveParams(Request $request)
     {
