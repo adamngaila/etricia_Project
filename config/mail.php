4e5cd378e326ebc3630a692894c994ed446f2edf
ADAM MWANGAILA <adamngaila@gmail.com>
2023-09-13 01:04:17 +0300

    new updates

--- config/mail.php
+++ config/mail.php
@@ -86,7 +86,7 @@ return [
 
     'username' => 'etriciatz@gmail.com',
 
-    'password' => 'bilm rkca bjml yabg',
+    'password' => 'ylof pgwf onkw ckye',
 
     /*
     |--------------------------------------------------------------------------
