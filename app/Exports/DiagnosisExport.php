fb437e7bec6ecba327fc159eb4d11453a2ebedb6
ADAM MWANGAILA <adamngaila@gmail.com>
2023-10-04 20:52:07 +0300

    new updates

--- app/Exports/DiagnosisExport.php
+++ app/Exports/DiagnosisExport.php
@@ -6,6 +6,7 @@ use Auth;
 use App\PackDiagnosisLogs;
 use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class DiagnosisExport implements FromCollection
 {
@@ -16,5 +17,21 @@ class DiagnosisExport implements FromCollection
     {
         $code = Auth::user()->serverip;
         return PackDiagnosisLogs::where('packagecode',$code)->where( 'created_at', '>', Carbon::now()->subDays(30))->get();
+    }
+     public function headings(): array
+    {
+        return [
+            'Id',
+            'Pack Code',
+            'Temp Sensors',
+            'Relay',
+            'Current Sensor',
+            'Voltage Sesnor',
+            'Invetor',
+            'Grid',
+            'Physical condition',
+            'time',
+            'time',
+        ];
     }
 }
